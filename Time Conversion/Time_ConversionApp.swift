a978336f9f464b8c3bb1c0af6b3a9ceb53d0f2d1
micahsteele <micahs66@gmail.com>
2022-11-29 13:46:50 -0500

    Initial Commit

---
+++ Time Conversion/Time_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ConversionApp.swift
+//  Time Conversion
+//
+//  Created by Micah Steele on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Time_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
